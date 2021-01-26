5a47fef1ddb661fbcf26d957656a4213d33d211e
Bansaj Pradhan <bansajpradhan@gmail.com>
2021-01-26 11:50:50 +0545

    ghorai updatee

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,12 +1,14 @@
 // Load the Visualization API and the corechart package.
 google.charts.load('current', {'packages':['corechart']});
 // Set a callback to run when the Google Visualization API is loaded.
-google.charts.setOnLoadCallback(drawChart);
+// google.charts.setOnLoadCallback(drawChart);
 
 // Callback that creates and populates a data table,
 // instantiates the pie chart, passes in the data and
 // draws it.
 function drawChart() {
+  console.log('drawing charts');
+  console.log(activeStatisticsJson);
   // Create the data table.
   var data = new google.visualization.DataTable();
   data.addColumn('string', 'Male');
--- assets/js/map.js
+++ assets/js/map.js
@@ -9,10 +9,10 @@ loadMap();
 //Call functions after map is loaded
 map.on('load', function () {
   addControls();
-  loadButwalGeoJson();
+  loadGhoraiGeoJson();
   loadInstitutionMarkers();
   removeInstitutionMarkers();
-  loadButwalAllWardStatistics();
+  loadGhoraiAllWardStatistics();
 });
 
 //Load Map function Declaration
@@ -22,8 +22,8 @@ function loadMap() {
     container: 'map',
     // style: 'mapbox://styles/mapbox/streets-v11', //Change styles
     style: 'mapbox://styles/bansajtest/ckk20h14m1j7417mzf0os2jq4',
-    center: [83.3623242, 27.6821973],
-    zoom: 11,
+    center: [82.4841966, 28.0392923],
+    zoom: 10,
   });
 }
 
@@ -33,43 +33,42 @@ function addControls() {
 }
 
 //add geojson fetched from the server to map
-function loadButwalGeoJson() {
-  $.getJSON(apiBaseUrl+"/butwal.php", function(mapJson) {
-    for (i=0;i<mapJson.features.length;i++) {
-      mapJson.features[i].properties.title_en = Number(mapJson.features[i].properties.title_en);
-    }
+function loadGhoraiGeoJson() {
+  $.getJSON(apiBaseUrl+"/ghorai.php", function(mapJson) {
+    // for (i=0;i<mapJson.features.length;i++) {
+    //   mapJson.features[i].properties.title_en = Number(mapJson.features[i].properties.title_en);
+    // }
     var geoColorStops = [
       [0, '#b60a1c'],
-      [1, '#e03531'],
-      [2, '#ff684c'],
-      [3, '#e39802'],
-      [4, '#f0bd27'],
-      [5, '#ffda66'],
-      [6, '#51b364'],
-      [7, '#8ace74']
+      [1000, '#e03531'],
+      [2000, '#ff684c'],
+      [3000, '#e39802'],
+      [4000, '#f0bd27'],
+      [5000, '#ffda66'],
+      [6000, '#51b364'],
+      [7000, '#8ace74']
     ];
-    console.log(mapJson);
-    map.addSource('butwal', {
+    map.addSource('ghorai', {
       'type': 'geojson',
       'data': mapJson
     });
     map.addLayer({
-      'id': 'butwal',
+      'id': 'ghorai',
       'type': 'fill',
-      'source': 'butwal',
+      'source': 'ghorai',
       'layout': {},
       'paint': {
         'fill-color': {
-          property: 'title_en',
+          property: 'population',
           stops: geoColorStops
         },
         'fill-opacity': 0.8,
       }
     });
     map.addLayer({
-      'id': 'butwal-ward-border',
+      'id': 'ghorai-ward-border',
       'type': 'line',
-      'source': 'butwal',
+      'source': 'ghorai',
       'paint': {
         'line-color': 'red',
         'line-width': 2
@@ -103,24 +102,20 @@ function removeInstitutionMarkers() {
   }
 }
 
-function loadButwalAllWardStatistics() {
-  $.getJSON(apiBaseUrl+"/read_statistic.php?municipality=136", function(json) {
-      statisticsJson = json.results; //local variable
-      for (let i = 0; i < statisticsJson.length; i++) {
-        totalStatisticsJson.population = totalStatisticsJson.male_population+parseInt(statisticsJson[i].population);
-        totalStatisticsJson.male_population = totalStatisticsJson.male_population+parseInt(statisticsJson[i].male_population);
-        totalStatisticsJson.female_population = totalStatisticsJson.female_population+parseInt(statisticsJson[i].female_population);
-        totalStatisticsJson.household = totalStatisticsJson.household+parseInt(statisticsJson[i].household);
-      }
-      activeStatisticsJson = totalStatisticsJson;
-      document.getElementById('household').innerHTML = totalStatisticsJson.household;
-      document.getElementById('male_population').innerHTML = totalStatisticsJson.male_population;
-      document.getElementById('female_population').innerHTML = totalStatisticsJson.female_population;
-    });
-    // drawChart();
-    map.on('mouseenter', 'butwal', onMouseEnter);
-    map.on('mouseleave', 'butwal', onMouseExit);
-    map.on('mousemove', 'butwal', onMouseMove);
+function loadGhoraiAllWardStatistics() {
+  $.getJSON(apiBaseUrl+"/read_statistic.php?municipality=193", function(json) {
+    statisticsJson = json.results; //local variable
+    for (let i = 0; i < statisticsJson.length; i++) {
+      totalStatisticsJson.population = totalStatisticsJson.male_population+parseInt(statisticsJson[i].population);
+      totalStatisticsJson.male_population = totalStatisticsJson.male_population+parseInt(statisticsJson[i].male_population);
+      totalStatisticsJson.female_population = totalStatisticsJson.female_population+parseInt(statisticsJson[i].female_population);
+      totalStatisticsJson.household = totalStatisticsJson.household+parseInt(statisticsJson[i].household);
+    }
+    setActiveStatistics(totalStatisticsJson);
+  });
+  map.on('mouseenter', 'ghorai', onMouseEnter);
+  map.on('mouseleave', 'ghorai', onMouseExit);
+  map.on('mousemove', 'ghorai', onMouseMove);
 }
 
 
@@ -128,8 +123,7 @@ function onMouseExit() {
     
   map.getCanvas().style.cursor = '';
   popup.remove();
-  // activeStatisticsJson = totalStatisticsJson;
-  // drawChart();
+  setActiveStatistics(totalStatisticsJson);
 }
 
 function onMouseEnter(e) {
@@ -137,18 +131,18 @@ function onMouseEnter(e) {
 }
 
 function onMouseMove(e) {
-    console.log(e.lngLat);
+    // console.log(e.lngLat);
   // Updates the cursor to a hand (interactivity)
   map.getCanvas().style.cursor = 'pointer';
+  console.log(e.features);
   // Show the popup at the coordinates with some data
   popup.setLngLat(e.lngLat)
     .setHTML(checkEmpty(e.features[0].properties.title_en))
     .addTo(map);
-    var activeStatistics = [];
+    setActiveStatistics(e.features[0].properties);
 }
 
 function checkEmpty(info) {
-    
   let activeStatistics = [];
   for (let i = 0; i < statisticsJson.length; i++) {
       if (statisticsJson[i].ward_title.toString() == info) {
@@ -164,4 +158,14 @@ function checkEmpty(info) {
     popupInfo = info;
   }
   return (popupInfo) ? popupInfo : "No data";
+}
+
+
+function setActiveStatistics(statistics) {
+  activeStatisticsJson = statistics;
+  document.getElementById('household').innerHTML = statistics.household;
+  document.getElementById('male_population').innerHTML = statistics.male_population;
+  document.getElementById('female_population').innerHTML = statistics.female_population;
+  drawChart();
+  //loadStatistics
 }
